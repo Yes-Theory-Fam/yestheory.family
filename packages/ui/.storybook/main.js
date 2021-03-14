34cdfd435fdb4a0d10efff41f9fc728aff031eb3
geisterfurz007 <michelvonv@me.com>
2021-03-14 17:36:04 +0100

    chore: Remove unused code and comments

--- packages/ui/.storybook/main.js
+++ packages/ui/.storybook/main.js
@@ -22,8 +22,8 @@ module.exports = {
     reactDocgen: "react-docgen-typescript",
     reactDocgenTypescriptOptions: {
       shouldExtractLiteralValuesFromEnum: true,
-      propFilter: (prop) => ["label", "disabled"].includes(prop.name),
-      //      propFilter: (prop) => (prop.parent ? !/node_modules/.test(prop.parent.fileName) : true),
+      propFilter: (prop) =>
+        prop.parent ? !/node_modules/.test(prop.parent.fileName) : true,
     },
   },
   webpackFinal: async (config) => {
