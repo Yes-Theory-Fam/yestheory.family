9edbf1407eff6258702dff6a426d4e59ed9a02a1
geisterfurz007 <michelvonv@me.com>
2021-06-04 17:40:11 +0200

    feat: DI with typedi

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -5,7 +5,10 @@ import { buildSchema, Ctx, NonEmptyArray, Query, Resolver } from "type-graphql";
 import { Example } from "./__generated__/type-graphql";
 import koa from "koa";
 import { ApolloServer } from "apollo-server-koa";
-import { createServerLogger } from "./log";
+import { createServerLogger } from "./services/logging/log";
+import { Logger } from "./services/logging/logService";
+import winston from "winston";
+import { Container, Service } from "typedi";
 
 const logger = createServerLogger("src", "index");
 
@@ -13,10 +16,16 @@ interface PrismaContext {
   prisma: PrismaClient;
 }
 
+@Service()
 @Resolver(Example)
 class TestResolver {
+  constructor(
+    @Logger("index", "testresolver") private logger: winston.Logger
+  ) {}
+
   @Query(() => [Example])
   async examples(@Ctx() { prisma }: PrismaContext): Promise<Example[]> {
+    this.logger.info("Test");
     return await prisma.example.findMany();
   }
 }
@@ -26,7 +35,7 @@ const prisma = new PrismaClient();
 const main = async () => {
   // eslint-disable-next-line @typescript-eslint/ban-types
   const resolvers: NonEmptyArray<Function> = [TestResolver];
-  const schema = await buildSchema({ resolvers });
+  const schema = await buildSchema({ resolvers, container: Container });
 
   const port = process.env["BACKEND_PORT"] ?? 5000;
   const app = new koa();
