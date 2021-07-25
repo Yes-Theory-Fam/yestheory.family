ec952599322c490aab55ff096dbb1ffce964447e
geisterfurz007 <michelvonv@me.com>
2021-07-25 13:39:28 +0200

    feat: proper loading button

--- packages/web/components/buddyproject/button.tsx
+++ packages/web/components/buddyproject/button.tsx
@@ -9,20 +9,20 @@ export const BuddyProjectButton: FunctionalComponent = () => {
   const state = useBuddyProjectState();
   const [{ fetching }, signUp] = useSignUpMutation();
 
-  console.log(state);
-
-  if (!state.status)
+  if (!state.status) {
     return <Button onClick={navigateToLogin}>Log In With Discord</Button>;
+  }
+
   if (
     state.status === BuddyProjectStatus.Matched ||
     state.status === BuddyProjectStatus.SignedUp
   ) {
-    return <Button disabled>You already signed up</Button>;
+    return <Button disabled>You signed up!</Button>;
   }
 
   return (
-    <Button onClick={() => signUp()}>
-      Join the Buddy Project! {fetching && "Loading (scuffed)"}
+    <Button onClick={() => signUp()} isLoading={fetching}>
+      Join the Buddy Project!
     </Button>
   );
 };
