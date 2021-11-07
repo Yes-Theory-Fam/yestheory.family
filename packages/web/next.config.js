ff48b59e6e2ec4c06f584823539c6669ba9a50c2
geisterfurz007 <michelvonv@me.com>
2021-11-07 18:24:13 +0100

    (WIP) chore: update packages

--- packages/web/next.config.js
+++ packages/web/next.config.js
@@ -2,4 +2,6 @@ const withPreact = require("next-plugin-preact");
 const withTM = require("next-transpile-modules")(["@yestheory.family/ui"]);
 const withImages = require("next-images");
 
-module.exports = withImages(withPreact(withTM()));
+module.exports = withImages(
+  withPreact(withTM({ images: { disableStaticImages: true } }))
+);
