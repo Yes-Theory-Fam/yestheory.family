aaeb36673f19e0f224a0d45458ec842479022a0e
geisterfurz007 <michelvonv@me.com>
2022-05-15 13:27:50 +0200

    chore: Formatting

--- packages/web/components/buddyproject/context/context.tsx
+++ packages/web/components/buddyproject/context/context.tsx
@@ -18,24 +18,25 @@ interface BuddyProjectProviderProps {
   children: VNode;
 }
 
-export const BuddyProjectProvider: FunctionalComponent<BuddyProjectProviderProps> =
-  ({ children }) => {
-    const loggedIn = useLoggedIn();
-    const [{ data, fetching, error }] = useStateQuery();
+export const BuddyProjectProvider: FunctionalComponent<
+  BuddyProjectProviderProps
+> = ({ children }) => {
+  const loggedIn = useLoggedIn();
+  const [{ data, fetching, error }] = useStateQuery();
 
-    const emptyStatus = { status: undefined, buddy: undefined };
+  const emptyStatus = { status: undefined, buddy: undefined };
 
-    const value =
-      !loggedIn || error || fetching
-        ? emptyStatus
-        : data?.getBuddyProjectStatus ?? emptyStatus;
+  const value =
+    !loggedIn || error || fetching
+      ? emptyStatus
+      : data?.getBuddyProjectStatus ?? emptyStatus;
 
-    return (
-      <BuddyProjectContext.Provider value={value}>
-        {children}
-      </BuddyProjectContext.Provider>
-    );
-  };
+  return (
+    <BuddyProjectContext.Provider value={value}>
+      {children}
+    </BuddyProjectContext.Provider>
+  );
+};
 
 export const useBuddyProjectState = (): BuddyProjectProps => {
   return useContext(BuddyProjectContext);
