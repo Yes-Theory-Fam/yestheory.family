61889b430a4a4b255e30f44457a6f5808e3191a3
geisterfurz007 <geisterfurz007@users.noreply.github.com>
2022-05-15 15:16:43 +0200

    Update dependencies (#58)
    * chore: Upgrade yarn
    
    * chore: upgrade minor / patch dependencies
    
    * chore: Formatting
    
    * chore: fix eslint using temporary fork of eslint-config-preact
    
    * chore: update test snapshots
    
    * fix: upgrade next to canary to avoid server.browser.js errors (preact/compat is missing that file)

--- packages/ui/src/components/common/ScrollToActionContainer/ScrollToActionContainer.tsx
+++ packages/ui/src/components/common/ScrollToActionContainer/ScrollToActionContainer.tsx
@@ -9,48 +9,49 @@ export interface ScrollToActionContainerProps extends BoxProps {
   overlapContent?: boolean;
 }
 
-export const ScrollToActionContainer: FunctionalComponent<ScrollToActionContainerProps> =
-  (props) => {
-    const { text, children, overlapContent = false, ...rest } = props;
+export const ScrollToActionContainer: FunctionalComponent<
+  ScrollToActionContainerProps
+> = (props) => {
+  const { text, children, overlapContent = false, ...rest } = props;
 
-    const selfRef = useRef<HTMLDivElement>(null);
-    const navbarHeight = useNavbarHeight();
+  const selfRef = useRef<HTMLDivElement>(null);
+  const navbarHeight = useNavbarHeight();
 
-    const scrollToContent = () => {
-      const selfHeight = selfRef.current?.getBoundingClientRect().height ?? 0;
+  const scrollToContent = () => {
+    const selfHeight = selfRef.current?.getBoundingClientRect().height ?? 0;
 
-      window.scroll({
-        top: selfHeight - navbarHeight,
-        behavior: "smooth",
-      });
-    };
-
-    return (
-      <Box minH={"100vh"} {...rest} display={"flex"} ref={selfRef}>
-        <Flex
-          w={"full"}
-          direction={"column"}
-          justify={"space-between"}
-          align={"center"}
-          pt={navbarHeight}
-          position={overlapContent ? "relative" : undefined}
-        >
-          <Box />
-          {children}
-          <VStack
-            position={overlapContent ? "absolute" : undefined}
-            bottom={0}
-            color={"brand.800"}
-            cursor={"pointer"}
-            onClick={scrollToContent}
-            mb={3}
-          >
-            <Text textTransform={"uppercase"} fontSize={"xs"}>
-              {text}
-            </Text>
-            <ChevronDownIcon boxSize={6} />
-          </VStack>
-        </Flex>
-      </Box>
-    );
+    window.scroll({
+      top: selfHeight - navbarHeight,
+      behavior: "smooth",
+    });
   };
+
+  return (
+    <Box minH={"100vh"} {...rest} display={"flex"} ref={selfRef}>
+      <Flex
+        w={"full"}
+        direction={"column"}
+        justify={"space-between"}
+        align={"center"}
+        pt={navbarHeight}
+        position={overlapContent ? "relative" : undefined}
+      >
+        <Box />
+        {children}
+        <VStack
+          position={overlapContent ? "absolute" : undefined}
+          bottom={0}
+          color={"brand.800"}
+          cursor={"pointer"}
+          onClick={scrollToContent}
+          mb={3}
+        >
+          <Text textTransform={"uppercase"} fontSize={"xs"}>
+            {text}
+          </Text>
+          <ChevronDownIcon boxSize={6} />
+        </VStack>
+      </Flex>
+    </Box>
+  );
+};
--- packages/web/components/buddyproject/signup-success-modal.tsx
+++ packages/web/components/buddyproject/signup-success-modal.tsx
@@ -16,29 +16,30 @@ interface SignupSuccessModalProps {
   onClose: () => void;
 }
 
-export const SignupSuccessModal: FunctionalComponent<SignupSuccessModalProps> =
-  ({ isOpen, onClose }) => {
-    return (
-      <Modal isOpen={isOpen} onClose={onClose} isCentered>
-        <ModalOverlay />
-        <ModalContent>
-          <ModalHeader>Everything went well!</ModalHeader>
-          <ModalCloseButton />
+export const SignupSuccessModal: FunctionalComponent<
+  SignupSuccessModalProps
+> = ({ isOpen, onClose }) => {
+  return (
+    <Modal isOpen={isOpen} onClose={onClose} isCentered>
+      <ModalOverlay />
+      <ModalContent>
+        <ModalHeader>Everything went well!</ModalHeader>
+        <ModalCloseButton />
 
-          <ModalBody>
-            <Text>
-              You are now signed up to the Buddy Project! YesBot will message
-              you soon with your buddy and the questions. Until then, feel free
-              to explore our server!
-            </Text>
-          </ModalBody>
+        <ModalBody>
+          <Text>
+            You are now signed up to the Buddy Project! YesBot will message you
+            soon with your buddy and the questions. Until then, feel free to
+            explore our server!
+          </Text>
+        </ModalBody>
 
-          <ModalFooter>
-            <Button minWidth={24} onClick={onClose}>
-              Close
-            </Button>
-          </ModalFooter>
-        </ModalContent>
-      </Modal>
-    );
-  };
+        <ModalFooter>
+          <Button minWidth={24} onClick={onClose}>
+            Close
+          </Button>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  );
+};
