4fe724c62472592a500dea21fc0ae5d725968b9d
geisterfurz007 <michelvonv@me.com>
2022-08-15 18:58:27 +0200

    Use /graphql as API endpoint

--- packages/web/environment.d.ts
+++ packages/web/environment.d.ts
@@ -2,6 +2,5 @@ declare namespace NodeJS {
   export interface ProcessEnv {
     NODE_ENV: "development" | "production" | "test";
     SERVER_BACKEND_GRAPHQL_URL: string;
-    NEXT_PUBLIC_BACKEND_URL: string;
   }
 }
--- packages/web/next.config.js
+++ packages/web/next.config.js
@@ -1,9 +1,20 @@
 const withTM = require("next-transpile-modules")(["@yestheory.family/ui"]);
 const withImages = require("next-images");
 
-module.exports = withImages(
-  withTM({
-    images: { disableStaticImages: true },
-    pageExtensions: ["page.tsx"],
-  })
-);
+/** @type {import("next").NextConfig} */
+const config = {
+  images: { disableStaticImages: true },
+  pageExtensions: ["page.tsx"],
+  rewrites: () => [
+    {
+      source: "/oauth/*",
+      destination: "http://localhost:5000/oauth/",
+    },
+    {
+      source: "/graphql",
+      destination: "http://localhost:5000/graphql",
+    },
+  ],
+};
+
+module.exports = withImages(withTM(config));
