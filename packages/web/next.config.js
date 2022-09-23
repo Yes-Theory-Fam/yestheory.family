13bce48ed6f63527e0f609f1fc2b432e3e6d8de6
geisterfurz007 <michelvonv@me.com>
2022-09-23 16:02:00 +0200

    CSP

--- packages/web/next.config.js
+++ packages/web/next.config.js
@@ -1,8 +1,10 @@
+const { PHASE_DEVELOPMENT_SERVER } = require("next/constants");
+
 const withTM = require("next-transpile-modules")(["@yestheory.family/ui"]);
 const withImages = require("next-images");
 
-/** @type {import("next").NextConfig} */
-const config = {
+/** @returns {import("next").NextConfig} */
+const config = (phase) => ({
   images: { disableStaticImages: true },
   pageExtensions: ["page.tsx"],
   productionBrowserSourceMaps: true,
@@ -16,6 +18,20 @@ const config = {
       destination: "http://localhost:5000/oauth/:slug*",
     },
   ],
-};
+  headers: () => [
+    {
+      source: "/:path*",
+      headers: [
+        { key: "X-Frame-Options", value: "DENY" },
+        {
+          key: "Content-Security-Policy",
+          value: `default-src 'self'; img-src 'self' https://cdn.discordapp.com/avatars/ data:; child-src 'none'; script-src 'self' ${
+            phase === PHASE_DEVELOPMENT_SERVER ? "'unsafe-eval'" : ""
+          }; style-src 'self' 'unsafe-inline'; frame-ancestors 'none'`,
+        },
+      ],
+    },
+  ],
+});
 
-module.exports = withImages(withTM(config));
+module.exports = (...args) => withImages(withTM(config(...args)));
