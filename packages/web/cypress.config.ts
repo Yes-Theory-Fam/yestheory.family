b856f8da0a9b9f9cb6b7a386d261a7d3851f6912
geisterfurz007 <geisterfurz007@users.noreply.github.com>
2022-11-12 15:24:16 +0100

    chore/more deps (#156)
    * chore: remove unused -loader packages
    
    * chore: upgrade koa packages; remove sirv-cli
    
    * Update some locks
    
    * chore: update cypress image
    
    * chore: update react-test-renderer
    
    * chore: upgrade framer-motion
    
    * chore: upgrade docker github actions
    
    * chore: upgrade yarn
    
    * chore: update typescript lock
    
    * fix: migrate cypress
    
    * chore: format cypress config
    
    * fix: mount correct cypress config
    
    * fix: require / import lint error

---
+++ packages/web/cypress.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from "cypress";
+import setup from "./cypress/plugins";
+
+export default defineConfig({
+  chromeWebSecurity: false,
+  experimentalStudio: true,
+  e2e: {
+    // We've imported your old cypress plugins here.
+    // You may want to clean this up later by importing these.
+    setupNodeEvents(on, config) {
+      setup(on, config);
+    },
+    baseUrl: "http://web:3000",
+  },
+});
