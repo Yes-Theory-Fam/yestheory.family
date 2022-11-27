a749ee5b5fd84aaee01557d9fc728fba785cc97f
geisterfurz007 <michelvonv@me.com>
2022-11-27 19:38:48 +0100

    fix: add note about not being affiliated with YT

--- packages/ui/src/components/common/Footer/Footer.tsx
+++ packages/ui/src/components/common/Footer/Footer.tsx
@@ -1,5 +1,6 @@
 import { FC } from "react";
 import { VStack } from "@chakra-ui/react";
+import { AffiliationExclusion } from "./components/AffiliationExclusion";
 import { Copyright } from "./components/Copyright";
 import {
   FooterLinkRow,
@@ -17,6 +18,7 @@ export const Footer: FC<FooterProps> = ({ links }) => (
   <VStack bg={"white"} py={4}>
     <Logo size={["sm", null, "md"]} />
     <FooterLinkRow links={links} />
+    <AffiliationExclusion />
     <Copyright />
   </VStack>
 );
---
+++ packages/ui/src/components/common/Footer/components/AffiliationExclusion.tsx
@@ -0,0 +1,10 @@
+import { Text } from "@chakra-ui/react";
+import { FC } from "react";
+
+export const AffiliationExclusion: FC = () => {
+  return (
+    <Text>
+      This website is in no way affiliated with or endorsed by Yes Theory.
+    </Text>
+  );
+};
