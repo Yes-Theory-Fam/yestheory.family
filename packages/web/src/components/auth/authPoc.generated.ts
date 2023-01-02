8214845574581ae7930fedbb3c1c3624d21e2075
geisterfurz007 <michelvonv@me.com>
2023-01-02 15:51:51 +0100

    fix: closed DMs show error modal on signup

--- packages/web/src/components/auth/authPoc.generated.ts
+++ packages/web/src/components/auth/authPoc.generated.ts
@@ -1,29 +1,24 @@
-import * as Types from "../../__generated__/types";
+import * as Types from '../../__generated__/types';
 
-import { gql } from "urql";
-import * as Urql from "urql";
+import { gql } from 'urql';
+import * as Urql from 'urql';
 export type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
 
-export type AuthPocQueryVariables = Types.Exact<{ [key: string]: never }>;
+export type AuthPocQueryVariables = Types.Exact<{ [key: string]: never; }>;
 
-export type AuthPocQuery = {
-  __typename?: "Query";
-  me?: { __typename?: "AuthenticatedUser"; username: string } | null;
-};
+
+export type AuthPocQuery = { __typename?: 'Query', me?: { __typename?: 'AuthenticatedUser', username: string } | null };
+
+
 
 export const AuthPocDocument = gql`
-  query AuthPoc {
-    me {
-      username
-    }
+    query AuthPoc {
+  me {
+    username
   }
-`;
-
-export function useAuthPocQuery(
-  options?: Omit<Urql.UseQueryArgs<AuthPocQueryVariables>, "query">
-) {
-  return Urql.useQuery<AuthPocQuery, AuthPocQueryVariables>({
-    query: AuthPocDocument,
-    ...options,
-  });
 }
+    `;
+
+export function useAuthPocQuery(options?: Omit<Urql.UseQueryArgs<AuthPocQueryVariables>, 'query'>) {
+  return Urql.useQuery<AuthPocQuery, AuthPocQueryVariables>({ query: AuthPocDocument, ...options });
+};
