564d66c68e0836b8482ef538595d12433eadf3c3
geisterfurz007 <michelvonv@me.com>
2023-06-02 18:01:48 +0200

    fix: cms connecting to typesense

--- packages/cms/src/lib/typesense.ts
+++ packages/cms/src/lib/typesense.ts
@@ -1,13 +1,19 @@
 import { Client } from "typesense";
 
+const apiUrl = new URL(
+  process.env.TYPESENSE_API_URL ?? "http://localhost:8108"
+);
+
+const protocol = apiUrl.protocol;
+
 export const typesenseClient = new Client({
   nodes: [
     {
-      host: "localhost",
-      port: 8108,
-      protocol: "http",
+      host: apiUrl.hostname,
+      port: Number(apiUrl.port),
+      protocol: protocol.substring(0, protocol.length - 1),
     },
   ],
-  apiKey: "1234567890",
+  apiKey: process.env.TYPESENSE_API_KEY ?? "1234567890",
   connectionTimeoutSeconds: 2,
 });
