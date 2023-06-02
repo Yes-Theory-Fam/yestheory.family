1e351e73ee5e2fb061341391569c2b9dc0ede25b
geisterfurz007 <michelvonv@me.com>
2023-06-02 17:01:00 +0200

    fix: support payload settings in deployment

--- packages/cms/src/init-payload.ts
+++ packages/cms/src/init-payload.ts
@@ -8,9 +8,18 @@ export const initPayload = async (additionalOptions?: Partial<InitOptions>) => {
   if (!process.env.PAYLOAD_SECRET) throw new Error("Missing PAYLOAD_SECRET");
   if (!process.env.MONGODB_URI) throw new Error("Missing MONGODB_URI");
 
+  const mongoOptions = process.env.MONGO_PASSWORD
+    ? {
+        authSource: "admin",
+        user: process.env.MONGO_USERNAME,
+        pass: process.env.MONGO_PASSWORD,
+      }
+    : undefined;
+
   return await payload.init({
     secret: process.env.PAYLOAD_SECRET,
     mongoURL: process.env.MONGODB_URI,
+    mongoOptions,
     ...additionalOptions,
   });
 };
