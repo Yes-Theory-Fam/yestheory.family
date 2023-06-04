389c849d0c6aa7b8f071ad245cca3afef95f1f2e
geisterfurz007 <michelvonv@me.com>
2023-06-04 14:24:25 +0200

    fix: using port 0

--- packages/web/src/lib/typesense.ts
+++ packages/web/src/lib/typesense.ts
@@ -17,16 +17,23 @@ export const getTypesenseClient = () => {
       process.env.TYPESENSE_DIRECT_URL ?? "http://localhost:8108"
     );
     const protocol = directUrl.protocol;
+    const port =
+      Number(directUrl.port) || (directUrl.protocol === "http" ? 80 : 443);
+
     node = {
       host: directUrl.hostname,
-      port: Number(directUrl.port),
+      port,
       protocol: protocol.substring(0, protocol.length - 1),
     };
   } else {
     const protocol = window.location.protocol;
+    const port =
+      Number(window.location.port) ||
+      (window.location.protocol === "http" ? 80 : 443);
+
     node = {
       host: window.location.hostname,
-      port: Number(window.location.port),
+      port: port,
       protocol: protocol.substring(0, protocol.length - 1),
       path: "/typesense",
     };
