ed02f65af81a1060fde4f49b8eed2d452aed57d7
geisterfurz007 <michelvonv@me.com>
2023-12-09 18:25:16 +0100

    fix: storybook linting not working properly

--- packages/web/eslint.config.js
+++ packages/web/eslint.config.js
@@ -1,9 +1,5 @@
 const storybookPlugin = require('eslint-plugin-storybook');
 
-const storybookRules = storybookPlugin.configs.recommended.overrides.map(
-  (conf) => ({...conf, plugins: {storybook: storybookPlugin}}),
-);
-
 /**
  * @type {import('eslint').Linter.FlatConfig[]}
  */
@@ -11,5 +7,21 @@ module.exports = [
   ...require('@atmina/linting/eslint/recommended'),
   require('@atmina/linting/eslint/tailwind'),
   require('@atmina/linting/eslint/next')(require('@next/eslint-plugin-next')),
-  ...storybookRules,
+  {
+    plugins: {storybook: storybookPlugin},
+    files: [
+      '**/*.stories.@(ts|tsx|js|jsx|mjs|cjs)',
+      '**/*.story.@(ts|tsx|js|jsx|mjs|cjs)',
+    ],
+    rules: {
+      ...storybookPlugin.configs.recommended.overrides[0].rules,
+    },
+  },
+  {
+    plugins: {storybook: storybookPlugin},
+    files: ['.storybook/main.@(js|cjs|mjs|ts)'],
+    rules: {
+      ...storybookPlugin.configs.recommended.overrides[1].rules,
+    },
+  },
 ];
