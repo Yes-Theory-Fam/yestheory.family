88ac64eb5bdf65bd2ac2a64fe6f9583b038f3f28
geisterfurz007 <michelvonv@me.com>
2023-12-23 23:57:55 +0100

    fix: polyfill util for passport because dunno how to trim out passport

--- packages/cms/src/plugins/externals.ts
+++ packages/cms/src/plugins/externals.ts
@@ -1,5 +1,6 @@
 import path from 'path';
 import {type Config} from 'payload/config';
+import {nodePolyfills} from 'vite-plugin-node-polyfills';
 
 const externalModules = ['autoprefixer', 'tailwindcss'];
 
@@ -24,8 +25,12 @@ export const externals = (config: Config) => {
 
     aliasArray.push(...replacements);
 
+    const plugins = viteConfig.plugins ?? [];
+    const pluginsWithPolyfills = [...plugins, nodePolyfills()];
+
     const merged = {
       ...viteConfig,
+      plugins: pluginsWithPolyfills,
       resolve: {...(viteConfig.resolve ?? {}), alias: aliasArray},
     };
 
