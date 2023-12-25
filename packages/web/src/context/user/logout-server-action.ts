0e59dd289bdd16b2f981e91a89fdb5a48c13f099
geisterfurz007 <michelvonv@me.com>
2023-12-25 19:14:36 +0100

    fix: logout

--- packages/web/src/context/user/logout-server-action.ts
+++ packages/web/src/context/user/logout-server-action.ts
@@ -1,12 +1,27 @@
 'use server';
 
+import {type ReadonlyRequestCookies} from 'next/dist/server/web/spec-extension/adapters/request-cookies';
 import {cookies} from 'next/headers';
 import {graphqlWithHeaders} from '../../lib/graphql/client';
+
+const isDevelopment = process.env.NODE_ENV === 'development';
+
+const deleteCookie = (cookies: ReadonlyRequestCookies, name: string) => {
+  cookies.set({
+    name,
+    value: '',
+    expires: 0,
+    maxAge: 0,
+    sameSite: 'none',
+    secure: !isDevelopment,
+    domain: '.yestheory.family',
+  });
+};
 
 export const logout = async () => {
   await graphqlWithHeaders((sdk) => sdk.Logout({}));
 
   const cks = cookies();
-  cks.delete('koa.sess');
-  cks.delete('koa.sess.sig');
+  deleteCookie(cks, 'koa.sess');
+  deleteCookie(cks, 'koa.sess.sig');
 };
