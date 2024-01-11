832a18ed318ffee1a56306837971df5bdf3dc2fc
geisterfurz007 <michelvonv@me.com>
2024-01-11 21:40:34 +0100

    feat: improve searchbar UX

--- packages/web/src/ui/groupchats/group-chat-search-bar/search-input.tsx
+++ packages/web/src/ui/groupchats/group-chat-search-bar/search-input.tsx
@@ -1,4 +1,4 @@
-import {MagnifyingGlassIcon} from '@heroicons/react/24/solid';
+import {MagnifyingGlassIcon} from '@heroicons/react/20/solid';
 import {type FC, useRef} from 'react';
 
 export type SearchInputProps = {
@@ -11,17 +11,19 @@ export const SearchInput: FC<SearchInputProps> = ({onChange}) => {
   const focusInput = () => inputRef.current?.focus();
 
   return (
-    <div className='flex rounded-md border-2 border-transparent bg-brand-50/50 transition-all focus-within:border-brand-800'>
+    <div className='group/searchbar flex rounded-md border-2 border-brand-800 focus-within:shadow-md'>
       <input
+        placeholder='Search a group'
         ref={inputRef}
         className='peer w-full bg-transparent px-3 py-2 outline-none'
         onChange={(e) => onChange(e.target.value)}
+        autoFocus
       />
       <div
         onClick={focusInput}
-        className='flex items-center justify-center p-2'
+        className='flex items-center justify-center px-3 py-2 transition-colors group-focus-within/searchbar:bg-brand-800'
       >
-        <MagnifyingGlassIcon className='h-4 w-4' />
+        <MagnifyingGlassIcon className='h-5 w-5 text-brand-800 transition-colors group-focus-within/searchbar:text-white' />
       </div>
     </div>
   );
