84dd078e6d4dfafa3dadab6f7789c703fa2155aa
geisterfurz007 <geisterfurz007@users.noreply.github.com>
2024-01-28 00:19:34 +0100

    fix: logout (#283)

--- packages/web/src/context/user/logout-server-action.ts
+++ packages/web/src/context/user/logout-server-action.ts
@@ -6,11 +6,11 @@ import {graphqlWithHeaders} from '../../lib/graphql/client';
 
 const deleteCookie = (cookies: ReadonlyRequestCookies, name: string) => {
   const url = new URL(process.env.FRONTEND_URL);
-  const domain = url.hostname;
+  const rootDomain = url.hostname.split('.').slice(-2).join('.');
 
   cookies.set({
     name,
-    domain,
+    domain: rootDomain,
     value: '',
     expires: 0,
     maxAge: 0,
