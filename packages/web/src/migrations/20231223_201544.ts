3ec2f0726cf51ae1dd50525d3734b23863ca510d
geisterfurz007 <michelvonv@me.com>
2024-08-23 23:36:53 +0200

    Fix migrations

--- packages/web/src/migrations/20231223_201544.ts
+++ packages/web/src/migrations/20231223_201544.ts
@@ -1,8 +1,8 @@
 import {
   type MigrateUpArgs,
   type MigrateDownArgs,
+  sql,
 } from '@payloadcms/db-postgres';
-import {sql} from 'drizzle-orm';
 
 export async function up({payload}: MigrateUpArgs): Promise<void> {
   await payload.db.drizzle.execute(sql`
